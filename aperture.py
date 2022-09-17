de78d7c172973a48bc2178a28cb03d56ef44a113
Brogan Bishop <brogan@dismail.de>
2022-09-17 12:56:32 -0500

    change course of action for missing preview file to update/generate

--- aperture.py
+++ aperture.py
@@ -668,7 +668,7 @@ class Aplib():
                 else:
                     test1 = self.location_of[uuid]
                     if not self.location_of[uuid].exists():
-                        raise Exception("GENERATE PREVIEWS")
+                        raise Exception("UPDATE/GENERATE PREVIEWS")
                     test2 = self.name_of[uuid]
                 if uuid in self.metadata:
                     vprint(path / (self.basename_of[uuid] + ".xmp"))
